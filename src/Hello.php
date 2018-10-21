8f238a116c1e23313b76bdb274fa0a9f0a7be0bf
3401889728@qq.com <3401889728@qq.com>
2018-10-21 10:09:13 +0800

    Signed-off-by:

---
+++ src/Hello.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 水主沉浮!
+ * Date: 2018/10/13
+ * Time: 12:51
+ * Q  Q: 3401889728
+ */
+namespace Hello;
+class Hello{
+    public function world()
+    {
+        echo "hello world";
+    }
+}
